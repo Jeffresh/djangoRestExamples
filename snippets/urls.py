6ea7bbfac7f9ff84c46795451dfd86faac915a8b
Jeff <jeff.uca@gmail.com>
2020-01-12 23:37:54 +0100

    [MOD] refactored to class based APIview
    refactored detail and list views using ApiView

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
+
+# function based
+# urlpatterns = [
+#     path('snippets/', views.snippet_list),
+#     path('snippets/<int:pk>/', views.snippet_detail),
+# ]
+
+# class based
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
+
+
+# We don't necessarily need to add these extra url patterns in,
+# but it gives us a simple, clean way of referring to a specific format.
+urlpatterns = format_suffix_patterns(urlpatterns)
