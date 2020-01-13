86532b3f707bd91acc9285394a5f2eceeb3f0d9e
Jeff <jeff.uca@gmail.com>
2020-01-13 13:16:55 +0100

    [MOD] urls to use ViewSets

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
+from snippets.views import SnippetViewSet, UserViewSet, api_root
+from rest_framework import renderers
 
 
 
@@ -9,6 +11,31 @@ from snippets import views
 #     path('snippets/', views.snippet_list),
 #     path('snippets/<int:pk>/', views.snippet_detail),
 # ]
+
+
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete':'destroy'
+})
+
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
 
 # API endpoints
 urlpatterns = [
@@ -19,26 +46,34 @@ urlpatterns = [
     # path('users/<int:pk>/', views.UserDetail.as_view()),
 
     path('', views.api_root),
+    #
+    # path('snippets/',
+    #      views.SnippetList.as_view(),
+    #      name='snippet-list'),
+    #
+    # path('snippets/<int:pk>/',
+    #      views.SnippetDetail.as_view(),
+    #      name='snippet-detail'),
+    #
+    # path('snippets/<int:pk>/highlight/',
+    #      views.SnippetHighlight.as_view(),
+    #      name='snippet-highlight'),
+    #
+    # path('users/',
+    #      views.UserList.as_view(),
+    #      name='user-list'),
+    #
+    # path('user/<int:pk>/',
+    #      views.UserDetail.as_view(),
+    #      name='user-detail')
 
-    path('snippets/',
-         views.SnippetList.as_view(),
-         name='snippet-list'),
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail')
 
-    path('snippets/<int:pk>/',
-         views.SnippetDetail.as_view(),
-         name='snippet-detail'),
-
-    path('snippets/<int:pk>/highlight/',
-         views.SnippetHighlight.as_view(),
-         name='snippet-highlight'),
-
-    path('users/',
-         views.UserList.as_view(),
-         name='user-list'),
-
-    path('user/<int:pk>/',
-         views.UserDetail.as_view(),
-         name='user-detail')
+    
 ]
 
 
