80a9f06d778fd3490541072145621fddc95077b1
Jeff <jeff.uca@gmail.com>
2020-01-13 09:11:11 +0100

    [MOD] refactored snippet detail view
    using mixins

--- snippets/views.py
+++ snippets/views.py
@@ -181,6 +181,7 @@ from rest_framework import generics
 class SnippetList(mixins.ListModelMixin,
                   mixins.CreateModelMixin,
                   generics.GenericAPIView):
+
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
@@ -189,3 +190,23 @@ class SnippetList(mixins.ListModelMixin,
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    
