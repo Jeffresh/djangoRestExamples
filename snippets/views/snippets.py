8cbd6812da9c7310c9e9402c3739db795c08dda7
Jeff <jeff.uca@gmail.com>
2020-01-16 10:29:17 +0100

    [MOD] Snippet view consume correctly API

--- snippets/views/snippets.py
+++ snippets/views/snippets.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from rest_framework.parsers import JSONParser
 import json
+import io
 import requests
 
 
 class SnippetList(View):
     def get(self, request):
-        snippets = requests.get("http://127.0.0.1:8000/api/snippets/")
+        snippets_request = requests.get("http://127.0.0.1:8000/api/snippets/")
+        stream_snippets = io.BytesIO(snippets_request.content)
+        deserialized_snippets = JSONParser().parse(stream_snippets)
 
-        return render(request, 'snippets.html', {'snippets': snippets.text})
+        data_snippets = []
+
+        for batch in deserialized_snippets['results']:
+            user_request = requests.get(batch['owner'])
+            stream_user = io.BytesIO(user_request.content)
+            deserialized_user = JSONParser().parse(stream_user)
+
+            data_snippets.append({'user': deserialized_user['username'], 'title': batch['title'], 'code': batch['code']})
+
+        return render(request, 'snippets.html', {'snippets': data_snippets})
